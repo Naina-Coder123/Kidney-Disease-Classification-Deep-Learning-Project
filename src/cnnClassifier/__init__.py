7533a56fbfea1e6ee356b58c085798d68d05b72c
Naina Mishra <2k23.csaiml2310558@gmail.com>
2026-01-20 13:49:54 +0530

    Added logger and common.py

---
+++ src/cnnClassifier/__init__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import logging
+
+
+os.makedirs("logs/running_logs", exist_ok=True)
+
+logging_Str="[%(asctime)s:%(levelname)s:%(module)s:%(message)s]"
+
+log_dir="logs"
+log_filepath=os.path.join(log_dir,"running_logs/log")
+os.makedirs(log_dir,exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_Str,
+    
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+        
+    ]
+)
+logger=logging.getLogger("cnnClassifierLogger")
