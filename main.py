4332805a99104cae91fa13cd100219f871f25fb6
Naina Mishra <2k23.csaiml2310558@gmail.com>
2026-01-23 16:34:09 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.cnnClassifier import logger
 
 from src.cnnClassifier.pipeline.stage01_data_ingestion import DataingestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 try:
@@ -11,3 +12,18 @@ try:
 except Exception as e:
      logger.exception(e)
      raise e
+ 
+ 
+ 
+
+ 
+STAGE_NAME="Prepare base model"
+if __name__ =='__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj=PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> {STAGE_NAME} completed <<<<<< \n\nx=========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
