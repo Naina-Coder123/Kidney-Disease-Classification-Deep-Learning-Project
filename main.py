ee118cfb16712450751fd368baccd76994896ea7
Naina Mishra <2k23.csaiml2310558@gmail.com>
2026-01-23 14:10:03 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.cnnClassifier import logger
 
-logger.info("Welcome to out custom log")
+from src.cnnClassifier.pipeline.stage01_data_ingestion import DataingestionTrainingPipeline
+
+STAGE_NAME="Data Ingestion Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj=DataingestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> {STAGE_NAME} completed <<<<<< \n\nx=========x")
+except Exception as e:
+     logger.exception(e)
+     raise e
