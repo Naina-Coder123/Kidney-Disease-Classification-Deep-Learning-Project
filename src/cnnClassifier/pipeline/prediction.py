5344b0925c9b4bae76392785921ee066b8e0e2fd
Naina Mishra <2k23.csaiml2310558@gmail.com>
2026-01-24 15:00:41 +0530

    model added

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -8,12 +8,12 @@ class PredictionPipeline:
         self.filename = filename
         
     def predict(self):
-        model = load_model(os.path.join("artifacts", "training", "model.h5"))
+        model = load_model(os.path.join("model", "model.h5"))
 
         test_image = image.load_img(self.filename, target_size=(128, 128))
         test_image = image.img_to_array(test_image)
         test_image = test_image / 255.0
-        test_image = np.expand_dims(test_image, axis=0)  # ✅ correct
+        test_image = np.expand_dims(test_image, axis=0)  
 
         prob = model.predict(test_image)[0][0]
 
