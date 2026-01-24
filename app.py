6d333ad82a57dcde26b4a04f8f0ae0c99ffde292
Naina Mishra <2k23.csaiml2310558@gmail.com>
2026-01-24 15:30:07 +0530

    procfile added

--- app.py
+++ app.py
@@ -40,11 +40,6 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp=ClientApp()
-    app.run(
-        host="127.0.0.1",
-        port=8081,   # change port
-        debug=False,
-        use_reloader=False
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
-    
